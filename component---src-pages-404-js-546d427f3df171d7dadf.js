35ab7a2126a8b7fe4dfe6a620a60bd66a89f020e
Daniel Pett <danielpett@gmail.com>
2023-01-04 10:07:57 +0000

    Updates

---
+++ component---src-pages-404-js-546d427f3df171d7dadf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmicropasts_blog_gatsby=self.webpackChunkmicropasts_blog_gatsby||[]).push([[883],{429:function(t,e,n){n.r(e),n.d(e,{Head:function(){return u}});var s=n(7294),a=n(1985),l=n(9357);const u=()=>s.createElement(l.Z,{title:"404: Not Found"});e.default=()=>s.createElement(a.Z,null,s.createElement("h1",null,"404: Not Found"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-546d427f3df171d7dadf.js.map
